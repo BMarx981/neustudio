b9623fe149f6b56bdb842cbf02c3c23c72ce5562
BMarx981 <b.marx981@gmail.com>
2023-12-31 10:44:54 -0500

    Create button and test.

--- lib/neustudio.dart
+++ lib/neustudio.dart
@@ -1,7 +1,43 @@
 library neustudio;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class NeuStudioButton extends StatefulWidget {
+  const NeuStudioButton({
+    super.key,
+    this.onTap,
+  });
+  final Function? onTap;
+
+  @override
+  State<NeuStudioButton> createState() => _NeuStudioButtonState();
+}
+
+class _NeuStudioButtonState extends State<NeuStudioButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () {
+            widget.onTap;
+          },
+          child: Container(
+            decoration: const BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(2, 2),
+                blurRadius: 2,
+              ),
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(-2, -2),
+                blurRadius: 2,
+              ),
+            ]),
+          ),
+        )
+      ],
+    );
+  }
 }
